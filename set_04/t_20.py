659fce6f67b7b7999e42b492b78114fca0ae91d3
Wenszel <wiktor.smaga13@gmail.com>
2022-11-11 19:35:08 +0100

    edited solution to tasks 20 from set 4

--- set_04/t_20.py
+++ set_04/t_20.py
@@ -1,17 +1,5 @@
 from random import randint
 
-
-def sum_row(array, row):
-    return sum(array[row])
-
-
-def sum_column(array, column, n):
-    output = 0
-    for i in range(n):
-        output = array[i][column]
-    return output
-
-
 n = int(input("n: "))
 t = [[randint(1, 20) for _ in range(n)] for _ in range(n)]
 for i in t:
@@ -20,6 +8,13 @@ for i in t:
     print("")
 
 t_sum_max = 0
+sc = [0 for _ in range(n)]
+sr = [0 for _ in range(n)]
+for x in range(n):
+    for y in range(n):
+        sc[x] += t[x][y]
+        sr[x] += t[y][x]
+
 for i in range(n ** 2):
     for j in range(i + 1, n ** 2):
         i_row = i // n
@@ -27,13 +22,13 @@ for i in range(n ** 2):
         j_row = j // n
         j_col = j % n
         if i_row == j_row:
-            t_sum = sum_row(t, i_row) + sum_column(t, i_col, n) + sum_column(t, j_col, n) \
-                    - 2 * t[i_row][i_col] - 2 * t[j_row][j_col]
+            t_sum = sr[i_row] + sc[i_col] + sc[j_col] \
+            - 2 * t[i_row][i_col] - 2 * t[j_row][j_col]
         elif i_col == j_col:
-            t_sum = sum_column(t, i_col, n) + sum_row(t, i_row) + sum_row(t, j_row) \
+            t_sum = sc[i_col] + sr[i_row] + sr[j_row] \
                     - 2 * t[i_row][i_col] - 2 * t[j_row][j_col]
         else:
-            t_sum = sum_column(t, i_col, n) + sum_column(t, j_col, n) + sum_row(t, i_row) + sum_row(t, j_row) \
+            t_sum = sc[i_col] + sc[j_col] + sr[i_row] + sr[j_row] \
                     - (2 * t[i_row][i_col] + 2 * t[j_row][j_col] + t[i_row][j_col] + t[j_row][i_col])
         t_sum_max = max(t_sum_max, t_sum)
 print(t_sum_max)
