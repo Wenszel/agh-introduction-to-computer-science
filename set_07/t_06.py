18171dd5ee3a8582a6c977f577310bf707abb26f
Wenszel <wiktor.smaga13@gmail.com>
2023-01-06 14:08:02 +0100

    added node initialization with array

--- set_07/t_06.py
+++ set_07/t_06.py
@@ -1,7 +1,17 @@
 class Node:
     def __init__(self, val=None):
-        self.val = val
-        self.next = None
+        if isinstance(val, list):
+            self.val = val[0]
+            new_node = Node(val[1])
+            self.next = new_node
+            pointer = new_node
+            for i in range(2, len(val)):
+                nested_node = Node(val[i])
+                pointer.next = nested_node
+                pointer = nested_node
+        else:
+            self.val = val
+            self.next = None
 
     def __repr__(self):
         return f"<Node val:{self.val} next:{self.next}>"
@@ -11,5 +21,3 @@ class Node:
         while head.next is not None:
             head = head.next
         head.next = Node(val)
-
-
